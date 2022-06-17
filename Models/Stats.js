5aa12299a47e86fc6812a8fe7ab15a85178cc05a
Jad Beydoun <104642512+Jayybee21@users.noreply.github.com>
2022-06-17 21:42:25 +0300

    Update Stats.js

--- Models/Stats.js
+++ Models/Stats.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 //the schema will define how will look like
@@ -31,37 +30,3 @@ const limniStats = mongoose.model(
 );
 
 module.exports = limniStats;
-=======
-const mongoose = require("mongoose");
-
-//the schema will define how will look like
-const statsSchema = new mongoose.Schema({
-  StatId: { type: Number },
-  Month: { type: String },
-  TotalRides: { type: String },
-  OngoingRides: { type: String },
-  CancelledRides: { type: String },
-  CompletedRides: { type: String },
-  TotalMonthyProfit: { type: String },
-  TotalRevenu: { type: String },
-  TotalDownloads: { type: String },
-  TotalUsers: { type: String },
-  TotalVisitors: { type: String },
-  OverallRatings: {
-    FiveStars: { type: String },
-    FourStars: { type: String },
-    ThreeStars: { type: String },
-    TwoStars: { type: String },
-    OneStar: { type: String },
-  },
-});
-
-//the model will represent the db in mongoDB and have it to our schame that we created here
-const limniStats = mongoose.model(
-  "LimniStatistics",
-  statsSchema,
-  "LimniStatistics"
-);
-
-module.exports = limniStats;
->>>>>>> 01f8580dd5fce21a04fc6ef7c1e5047d87685aee
