c246b2e0635d8635e394d500a9de80ac6e3173fa
Jad Beydoun <104642512+Jayybee21@users.noreply.github.com>
2022-06-17 21:42:03 +0300

    Update SharedRides.js

--- Models/SharedRides.js
+++ Models/SharedRides.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 //the schema will define how will look like
@@ -19,25 +18,3 @@ const SharedRidesModel = mongoose.model(
 );
 
 module.exports = SharedRidesModel;
-=======
-const mongoose = require("mongoose");
-
-//the schema will define how will look like
-const SharedRidesShema = new mongoose.Schema({
-  chaufeur: {
-    type: String,
-  },
-  client: {
-    type: String,
-  },
-});
-
-//the model will represent the db in mongoDB and have it to our schema that we created here
-const SharedRidesModel = mongoose.model(
-  "SharedRides",
-  SharedRidesShema,
-  "SharedRides"
-);
-
-module.exports = SharedRidesModel;
->>>>>>> 01f8580dd5fce21a04fc6ef7c1e5047d87685aee
