31f254a663c09682387aa37c07205dc0622808c3
Jad Beydoun <104642512+Jayybee21@users.noreply.github.com>
2022-06-17 21:42:42 +0300

    Update Users.js

--- Models/Users.js
+++ Models/Users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 //the schema will define how will look like
@@ -41,47 +40,3 @@ const userSchema = new mongoose.Schema({
 const userModel = mongoose.model("LimniUsers", userSchema, "LimniUsers");
 
 module.exports = userModel;
-=======
-const mongoose = require("mongoose");
-
-//the schema will define how will look like
-const userSchema = new mongoose.Schema({
-  type: {
-    genre: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    number: {
-      type: Number,
-    },
-    email: {
-      type: String,
-    },
-    car: {
-      type: String,
-    },
-    driverLicense: {
-      type: String,
-    },
-    driverLicensePlate: {
-      type: String,
-    },
-    image: {
-      type: String,
-    },
-  },
-});
-
-//the model will represent the db in mongoDB and have it to our schema that we created here
-const userModel = mongoose.model("LimniUsers", userSchema, "LimniUsers");
-
-module.exports = userModel;
->>>>>>> 01f8580dd5fce21a04fc6ef7c1e5047d87685aee
